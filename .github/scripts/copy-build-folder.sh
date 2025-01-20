6672610bb1a8be0abd52dd83a12f3464cf819981
Deirdre Lungley <dmlung@essex.ac.uk>
2025-01-20 11:11:21 +0000

    Amended copy script to create subfolder

--- .github/scripts/copy-build-folder.sh
+++ .github/scripts/copy-build-folder.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail # strict mode
-readonly SCRIPT_NAME="$(basename "$0")"
-function l { # Log a message to the terminal.
-    echo
-    echo -e "[$SCRIPT_NAME] ${1:-}"
-}
+
+DESTINATION_PATH=$DESTINATION_REPO$DESTINATION_TAG
 
 # Folders to copy from dmlung_test
 BUILD_E_FOLDER=./dmlung_test/build/encoding
@@ -12,16 +9,17 @@ BUILD_FLD_FOLDER=./dmlung_test/build/field-level-documentation
 
 # if the folder exists in dmlung_test, copy it to dmlung-test-io repo subfolder
 if [ -d "$BUILD_E_FOLDER" ]; then
-    echo "Copying $BUILD_E_FOLDER"
-    cp -R ./dmlung_test/build/encoding $DESTINATION_PATH/encoding
+    echo "Copying $BUILD_E_FOLDER to $DESTINATION_PATH"
+    mkdir $DESTINATION_PATH
+    cp -R $BUILD_E_FOLDER $DESTINATION_PATH
 fi
 
-echo "Build encoding folder copied to $DESTINATION_PATH subfolder"
+echo "$BUILD_E_FOLDER folder copied to $DESTINATION_PATH"
 
 # if the folder exists in dmlung_test, copy it to dmlung-test-io repo subfolder
 if [ -d "$BUILD_FLD_FOLDER" ]; then
-    echo "Copying $BUILD_FLD_FOLDER"
-    cp -R ./dmlung_test/build/field-level-documentation $DESTINATION_PATH/field-level-documentation
+    echo "Copying $BUILD_FLD_FOLDER to $DESTINATION_PATH"
+    cp -R $BUILD_FLD_FOLDER $DESTINATION_PATH
 fi
 
-echo "Build field-level-documentation folder copied to $DESTINATION_PATH subfolder"
+echo "$BUILD_FLD_FOLDER folder copied to $DESTINATION_PATH"
